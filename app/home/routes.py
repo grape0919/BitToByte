b37140c566aa51172b077e3bdc3113d169d986c1
Hongkyokim <ghdry2563@gmail.com>
2021-10-19 02:50:51 +0900

    completed ws

--- app/home/routes.py
+++ app/home/routes.py
@@ -3,30 +3,32 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+import json
 from app.home import blueprint
 from flask import render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
-from APICaller.src.up.pyUpbitTest import tickerAll
+from APICaller.src.up.pyUpbitTest import getCoinList, tickerAll
 
 @blueprint.route('/index')
 @login_required
 def index():
-
-    return render_template('index.html', segment='index', datas=tickerAll())
+    datas = getCoinList().split(",")
+    print("!@#!@# tickter : ", datas)
+    return render_template('index.html', segment='index', datas=json.dumps(datas), datas_list=datas)
 
 @blueprint.route('/alarmii')
 @login_required
 def alarmii():
     target = "KRW-BTC"
-    return render_template('alarmii.html', segment='alarmii', datas=tickerAll())
+    return render_template('alarmii.html', segment='alarmii', datas=getCoinList())
 
 @blueprint.route('/simulation')
 @login_required
 def simulation():
     target = "KRW-BTC"
-    return render_template('simulation.html', segment='simulation', datas=tickerAll())
+    return render_template('simulation.html', segment='simulation', datas=getCoinList())
 
 @blueprint.route('/<template>')
 @login_required
